9e118b8dbaf4833d206e0829006a5ebd75e61939
Sudhir Ranjan <ranjan.sudhir1@gmail.com>
2022-03-22 13:01:10 +0530

    Added Login Page design.

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -7,7 +7,7 @@ class Login extends StatelessWidget {
       title: "Login",
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Tets"),
+          title: const Text("Login"),
         ),
         body: const LoginForm(),
       ),
@@ -20,29 +20,68 @@ class LoginForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Padding(
-         padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-         child: TextFormField(
-           decoration: const InputDecoration(
-             border: UnderlineInputBorder(),
-             labelText: 'Email',
-           ),
-         ),
-       ),
-       Padding(
-         padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-         child: TextFormField(
-           obscureText: true,
-           decoration: const InputDecoration(
-             border: UnderlineInputBorder(),
-             labelText: 'Password',
-           ),
-         ),
-       ),
-     ],
+    return Container(
+      alignment: Alignment.center,
+
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+            child: Center(
+              child: Image(
+                image: AssetImage('assets/logo.png'),
+                fit: BoxFit.contain,
+                width: 200,
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 10.0),
+            child: const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+                child: Text(
+                  "Get access to your Orders, Wishlist and Recommendations",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                )),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Email',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: TextFormField(
+              obscureText: true,
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Password',
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(right: 20.0,left: 20.0,top: 10.0),
+            child: Center(
+              child: SizedBox(
+                width: double.infinity,
+                child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.pink,
+                      primary: Colors.white, // foreground
+                    ),
+                    onPressed: () {},
+                    child: const Text("Login")),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
